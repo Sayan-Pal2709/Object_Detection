ce82201d58702e0dc8b887b9d447e3a4058aa67d
Sayan-Pal2709 <160103187+Sayan-Pal2709@users.noreply.github.com>
2025-03-11 20:59:52 +0530

    update

--- pat2.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Enter a number: "))
-r = (n*2)-1 #n+n-1
-for i in range(1,r+1):
-    for j in range(1,r+1):
-        for p in range(1,n+1):
-            if(i==p or j==p or i==n+(n-p) or j==n+(n-p)):
-                print(f"{n-(p-1)} ",end = " ")
-                break
-    print()
